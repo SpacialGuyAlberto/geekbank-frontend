ba00a73a377d1e8f05965ef75a269aa9e158d28a
SpacialGuyAlberto <lalbertomurillo1996@gmail.com>
2024-11-17 15:01:08 +0100

    Compras con balance y redireccion

--- src/app/balance-payment/balance-payment.component.ts
+++ src/app/balance-payment/balance-payment.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, Output, EventEmitter, OnInit, OnDestroy, inject} from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {CurrencyPipe, NgIf} from '@angular/common';
+import {CurrencyPipe, NgForOf, NgIf} from '@angular/common';
 import { Subscription } from 'rxjs';
 import { OrderService } from '../order.service';
 import { NotificationService } from '../services/notification.service';
@@ -9,12 +9,13 @@ import { AccountService } from '../account.service';
 import { OrderRequest } from '../models/order-request.model';
 import { Account } from '../models/User';
 import {before} from "node:test";
+import { Router } from '@angular/router';
 import {CART_ITEMS, GAME_USER_ID, IS_MANUAL_TRANSACTION, PRODUCT_ID, TOTAL_PRICE} from "../payment/payment.token";
 
 @Component({
   selector: 'app-balance-payment',
   standalone: true,
-  imports: [FormsModule, NgIf, CurrencyPipe],
+  imports: [FormsModule, NgIf, CurrencyPipe, NgForOf],
   templateUrl: './balance-payment.component.html',
   styleUrls: ['./balance-payment.component.css'],
 })
@@ -46,7 +47,8 @@ export class BalancePaymentComponent implements OnInit, OnDestroy {
     private orderService: OrderService,
     private authService: AuthService,
     private accountService: AccountService,
-    private notificationService: NotificationService
+    private notificationService: NotificationService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
@@ -114,6 +116,15 @@ export class BalancePaymentComponent implements OnInit, OnDestroy {
         this.notifMessage = 'Tu compra fue completada con tu balance.';
         this.showSpinner = false;
         this.close.emit();
+        this.router.navigate(['/purchase-confirmation'], {
+          queryParams: {
+            orderRequestNumber: response.orderRequestNumber,
+            transactionNumber: response.transactionNumber,
+            transactionStatus: response.transactionStatus,
+            tempPin: response.tempPin,
+          },
+        });
+
       },
       error: (err) => {
         console.error('Error during purchase:', err);
