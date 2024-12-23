bc276ba579de56fe4cbcf03d7b1cef7d61281148
SpacialGuyAlberto <lalbertomurillo1996@gmail.com>
2024-12-23 23:33:02 +0100

    giftcard wish list

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://astralisbank.com:7070/api',
+  apiUrl: 'https://astralisbank.com/api',
   apiKey: '77d96c852356b1c654a80f424d67048f',  // Clave API para producción
   authDomain: 'https://astralisbank.com'
 };
