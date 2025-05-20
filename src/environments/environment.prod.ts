97a04ec90334de1bc545882caeadc87d4d3654ed
Ubuntu <ubuntu@ip-172-26-0-41.eu-central-1.compute.internal>
2025-05-20 20:37:48 +0000

    Stripe Token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'https://astralisbank.com/api',
   apiKey: '77d96c852356b1c654a80f424d67048f',  // Clave API para producción
-  authDomain: 'https://astralisbank.com'
+  authDomain: 'https://astralisbank.com',
+  stripeToken: 'pk_live_51RFhDhFjikDIxOfHEgc4MAHnitgb6AebPsYsNvdyJwid4ekGA7pxW0NVd3mQAS1gIN5YY3jZHGOEFkYPWd8D4yoU00IikKIuqo'
 };
 
